bc58fec2905993815a525bbe90ab8b84910f840c
ustundag <ustundaganil@gmail.com>
2020-03-18 17:22:14 +0300

    sent rectangle coordinates & fix frontend margins.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, HttpResponse
+import json
 
 # Create your views here.
 def index(request):
-    #return HttpResponse("index")
-    return render(request, "index.html")
+    if request.method == 'GET':
+        return render(request, "index.html")
+    elif request.method == 'POST':
+        data = request.POST.get('rect', None)
+        if isinstance(data, str):
+            data = json.loads(data)
+            print('////////////////////////////////////////')
+            print(data['startX'])
+            print(data['startY'])
+            print(data['w'])
+            print(data['h'])
+            print('////////////////////////////////////////')
+            objects = {}
+            objects['list'] = cnn_model()
+            return HttpResponse(json.dumps(objects))
+
+# neural network model for object prediction
+def cnn_model():
+    return 'django'
--- static/js/draw.js
+++ static/js/draw.js
@@ -4,6 +4,7 @@ var ctx = canvas.getContext("2d");
 var rect = {};
 var drag = false;
 var imageObj = null;
+var startX=0, startY=0, w=0, h=0;
 
 function init() {
     imageObj = new Image();
@@ -14,14 +15,13 @@ function init() {
     canvas.addEventListener('mousemove', mouseMove, false);
 }
 
+// mouse listeners
 function mouseDown(e) {
     rect.startX = e.pageX - this.offsetLeft;
     rect.startY = e.pageY - this.offsetTop;
     drag = true;
 }
-
 function mouseUp() { drag = false; }
-
 function mouseMove(e) {
     if (drag) {
         ctx.clearRect(0, 0, 500, 500);
@@ -30,8 +30,51 @@ function mouseMove(e) {
         rect.h = (e.pageY - this.offsetTop) - rect.startY;
         ctx.strokeStyle = 'red';
         ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+        startX = rect.startX;
+        startY = rect.startY;
+        w = rect.w;
+        h = rect.h;
     }
 }
 
-init();
+function post_coordinates(startX, startY, w, h){
+    var rect = {
+        'startX': 10,
+        'startY': 20,
+        'w': 30,
+        'h': 40
+    };
+    let csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            // if not safe, set csrf_token
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+    $.ajax({
+        url:"",
+        type: "POST",
+        headers: [{name: 'X-CSRFToken', value: '{{ csrf_token }}'}],
+        data: {'rect': JSON.stringify(rect)},
+        dataType: 'json',
+        success:function(data){
+            console.log('success');
+        },
+        error:function (xhr, textStatus, thrownError){
+            console.log(thrownError);
+        }
+    });
+}
+
+$('button').click(function(e) {
+    post_coordinates(rect.startX, rect.startY, rect.w, rect.h);
 });
+
+init();
+});//document ready!
